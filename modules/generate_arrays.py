c2cc2b596caa00111ef5c970b8732c7496c5f4ce
radie <Radievskiy2003@gmail.com>
2023-09-16 14:00:18 +0300

    Done lab 1

--- modules/generate_arrays.py
+++ modules/generate_arrays.py
@@ -3,17 +3,13 @@ import numpy as np
 class ArrayGenerator:
     @staticmethod
     def random_arrays():
-        # Код для генерації випадкових масивів
         print("Генерація випадкових масивів:")
-        # Пример генерации массива
         arr = np.random.randint(0, 100, 10)
         print(arr)
 
     @staticmethod
     def non_random_arrays():
-        # Код для генерації невипадкових масивів
         print("Генерація невипадкових масивів:")
-        # Пример генерации массива
         arr = np.arange(10)
         print(arr)
 
