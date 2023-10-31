92319edd3912766d9d69ccccbc9c17f6f84aee65
radie <Radievskiy2003@gmail.com>
2023-11-01 01:29:03 +0200

    Done lab 6

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 # 1. Читання файлу
 data = pd.read_json('data/Version 5.json')
 
-# Зміна назв стовпців (якщо потрібно)
+# Зміна назв стовпців
 data.columns = ['ID', 'Warehouse_block', 'Mode_of_Shipment', 'Customer_care_calls', 'Customer_rating',
                 'Cost_of_the_Product', 'Prior_purchases', 'Product_importance', 'Gender', 'Discount_offered',
                 'Weight_in_gms', 'Reached_on_Time']
@@ -17,13 +17,20 @@ if duplicates.sum() > 0:
 
 # Перевірка на відсутні значення
 missing_values = data.isnull().sum()
-print(missing_values)
+print("Відсутні значення:\n", missing_values)
 
 # Заповнення відсутніх значень
-# Наприклад, якщо у нас є відсутні значення в стовпці 'Customer_care_calls', ми можемо заповнити їх середнім значенням
 if missing_values['Customer_care_calls'] > 0:
     mean_value = data['Customer_care_calls'].mean()
     data['Customer_care_calls'].fillna(mean_value, inplace=True)
 
+# Перевірка статистичних даних
+print("\nСтатистичні дані:\n", data.describe())
+
+# Перевірка унікальних значень категоріальних змінних
+categorical_columns = ['Warehouse_block', 'Mode_of_Shipment', 'Product_importance', 'Gender', 'Reached_on_Time']
+for column in categorical_columns:
+    print(f"\nУнікальні значення для {column}:\n", data[column].value_counts())
+
 # Виведення перших 5 рядків даних для перевірки
-print(data.head())
+print("\nПерші 5 рядків даних:\n", data.head())
